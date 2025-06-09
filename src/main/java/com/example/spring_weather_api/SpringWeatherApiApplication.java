d419e08f2875898f68e128df10c46dcdd25ebbf4
gregor-uusvali <gregoruusvali@gmail.com>
2025-06-09 20:45:07 +0300

    init

---
+++ src/main/java/com/example/spring_weather_api/SpringWeatherApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_weather_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWeatherApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWeatherApiApplication.class, args);
+	}
+
+}
