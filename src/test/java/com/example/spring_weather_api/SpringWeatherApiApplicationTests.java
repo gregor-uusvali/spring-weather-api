37f39e182cd4d53b889049ece967228414dc0417
gregor-uusvali <gregoruusvali@gmail.com>
2025-06-09 23:20:21 +0300

    test 1

--- src/test/java/com/example/spring_weather_api/SpringWeatherApiApplicationTests.java
+++ src/test/java/com/example/spring_weather_api/SpringWeatherApiApplicationTests.java
@@ -1,13 +1,38 @@
 package com.example.spring_weather_api;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.spring_weather_api.domain.CityCordinates;
+import com.example.spring_weather_api.entity.GeocodingCordinatesEntity;
+import com.example.spring_weather_api.transformer.GeocordinatesTransformer;
 
 @SpringBootTest
 class SpringWeatherApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testGeocordinatesTransformer() {
+		final GeocordinatesTransformer geocodingCordinatesTransformer = new GeocordinatesTransformer();
+		final GeocodingCordinatesEntity geocodingCordinatesEntity = new GeocodingCordinatesEntity();
+		GeocodingCordinatesEntity.builder()
+				.latitude("123")
+				.longitude("345")
+				.build();
+
+		final CityCordinates cityCordinates = geocodingCordinatesTransformer.transformToDomain(geocodingCordinatesEntity);
+
+		assertAll("Should return a city cordinates",
+				() -> assertEquals(geocodingCordinatesEntity.getLatitude(), cityCordinates.getLatitude()),
+				() -> assertEquals(geocodingCordinatesEntity.getLongitude(), cityCordinates.getLongitude()));
+
 	}
 
 }
